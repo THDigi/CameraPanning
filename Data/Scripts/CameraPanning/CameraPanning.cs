8397122b41035d0d3cacb2e238c6a854bd2f83ab
Digi <hunter_digital_sl@yahoo.com>
2017-09-21 21:46:24 +0300

    Added reset view for 1st person only worlds

--- Data/Scripts/CameraPanning/CameraPanning.cs
+++ Data/Scripts/CameraPanning/CameraPanning.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Sandbox.Common.ObjectBuilders;
 using Sandbox.Definitions;
 using Sandbox.Game;
@@ -12,7 +11,6 @@ using VRage.Game.ModAPI;
 using VRage.Input;
 using VRage.ModAPI;
 using VRage.ObjectBuilders;
-using VRage.Utils;
 using VRageMath;
 
 namespace Digi.CameraPanning
@@ -28,6 +26,7 @@ namespace Digi.CameraPanning
 
         public static CameraPanningMod instance = null;
         private bool init = false;
+        private bool thirdPersonEnabled = false;
         private float originalCameraFovSmall = 0;
         private float originalCameraFovLarge = 0;
 
@@ -49,6 +48,8 @@ namespace Digi.CameraPanning
                 init = true;
                 Log.Init();
 
+                thirdPersonEnabled = MyAPIGateway.Session.SessionSettings.Enable3rdPersonView;
+
                 var def = GetCameraDefinition(CAMERA_SMALL_ID);
 
                 if(def != null)
@@ -67,6 +68,40 @@ namespace Digi.CameraPanning
 
                 // SetUpdateOrder() throws an exception if called in the update method; this to overcomes that
                 MyAPIGateway.Utilities.InvokeOnGameThread(() => SetUpdateOrder(MyUpdateOrder.NoUpdate));
+            }
+            catch(Exception e)
+            {
+                Log.Error(e);
+            }
+        }
+
+        public override void HandleInput()
+        {
+            try
+            {
+                if(!init || thirdPersonEnabled || MyAPIGateway.Gui.IsCursorVisible || MyAPIGateway.Gui.ChatEntryVisible)
+                    return;
+
+                if(MyAPIGateway.Input.IsNewGameControlPressed(MyControlsSpace.CAMERA_MODE))
+                {
+                    var camCtrl = MyAPIGateway.Session.CameraController;
+                    var controller = MyAPIGateway.Session.ControlledObject as Sandbox.Game.Entities.IMyControllableEntity; // avoiding ambiguity
+
+                    if(camCtrl == null || controller == null)
+                        return;
+
+                    if(controller is IMyShipController)
+                    {
+                        // HACK this is how MyCockpit.Rotate() does things so I kinda have to use these magic numbers.
+                        var num = MyAPIGateway.Input.GetMouseSensitivity() * 0.13f;
+                        camCtrl.Rotate(new Vector2(controller.HeadLocalXAngle / num, controller.HeadLocalYAngle / num), 0);
+                    }
+                    else
+                    {
+                        // HACK this is how MyCharacter.RotateHead() does things so I kinda have to use these magic numbers.
+                        camCtrl.Rotate(new Vector2(controller.HeadLocalXAngle * 2, controller.HeadLocalYAngle * 2), 0);
+                    }
+                }
             }
             catch(Exception e)
             {
@@ -301,7 +336,7 @@ namespace Digi.CameraPanning
                     }
                 }
             }
-            
+
             if(!MyAPIGateway.Gui.ChatEntryVisible && !MyAPIGateway.Gui.IsCursorVisible)
             {
                 if(!recenter && cameraModeControl.IsNewPressed())
